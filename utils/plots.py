315baf1358bb29de6ed2bd75b41462888bf6fea4
davidsvaughn <davidsvaughn@gmail.com>
2021-04-27 16:15:45 -0400

    forgot to update plots.py

--- utils/plots.py
+++ utils/plots.py
@@ -83,6 +83,7 @@ def plot_one_box(x, img, color=None, label=None, line_thickness=None):
     color = color or [random.randint(0, 255) for _ in range(3)]
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
     cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    # cv2.rectangle(img, c1, c2, color, tl, cv2.LINE_AA)
     if label:
         tf = max(tl - 1, 1)  # font thickness
         t_size = cv2.getTextSize(label, 0, fontScale=tl / 2, thickness=tf)[0]
@@ -92,6 +93,21 @@ def plot_one_box(x, img, color=None, label=None, line_thickness=None):
         oy = max(12, oy)
         cv2.putText(img, label, (ox, oy), 0, tl / 2, [0, 0, 0], thickness=tf, lineType=cv2.LINE_AA)
         # print((ox, oy))
+
+def crop_one_box(x, img):
+    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+    return img[c1[1]:c2[1], c1[0]:c2[0]]
+
+def plot_one_label(label, img, ox, oy, color=None, line_thickness=None):
+    # Plots one bounding box on image img
+    tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
+    color = color or [random.randint(0, 255) for _ in range(3)]
+    tf = max(tl - 1, 1)  # font thickness
+    t_size = cv2.getTextSize(label, 0, fontScale=tl / 2, thickness=tf)[0]
+    # ox, oy = c1[0], c1[1] - 2
+    oy = max(12, oy)
+    cv2.putText(img, label, (ox, oy), 0, tl / 2, color, thickness=tf, lineType=cv2.LINE_AA)
+    # print((ox, oy))
 
 def plot_one_box_PIL(box, img, color=None, label=None, line_thickness=None):
     img = Image.fromarray(img)
