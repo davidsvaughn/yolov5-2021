2cdbfc4ce9aedefd2e1cb4519a18ee3d1739c58e
d.vaughn <d.vaughn@precisionhawk.com>
2022-02-04 15:56:22 -0500

    DDP testing

--- train.py
+++ train.py
@@ -278,10 +278,11 @@ def train(hyp, opt, device, tb_writer=None):
 
 
     # Testloader
-    # test_batch_size = batch_size
-    test_batch_size = opt.world_size ## so test_batch_size-per-GPU = 1 (needed for DDP validation)
+    test_batch_size = batch_size
+    # test_batch_size = opt.world_size ## so test_batch_size-per-GPU = 1 (needed for DDP validation)
     if rank in [-1, 0]:
-        pfunc(f'!!!!!!!! SETTING test_batch_size = opt.world_size = {opt.world_size}')
+        pfunc(f'!!!!!!!! test_batch_size = {test_batch_size}')
+        # pfunc(f'!!!!!!!! SETTING test_batch_size = opt.world_size = {opt.world_size}')
     testloader = create_dataloader(test_path, imgsz_test, test_batch_size, gs, opt,  # testloader
                                     hyp=hyp, cache=opt.cache_images and not opt.notest, rect=True, rank=rank,
                                     world_size=opt.world_size, workers=opt.workers,
