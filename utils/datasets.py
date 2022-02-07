bbb972c112331c965b9cdcd663e2ec722ea8d604
d.vaughn <d.vaughn@precisionhawk.com>
2022-02-07 14:28:45 -0500

    DDP testing

--- utils/datasets.py
+++ utils/datasets.py
@@ -62,20 +62,20 @@ def create_dataloader(path, imgsz, batch_size, stride, opt, hyp=None, augment=Fa
     lazy_caching = False
     cache_efficient_sampling = True
 
-    # with torch_distributed_zero_first(rank):
-    dataset = LoadImagesAndLabels(path, imgsz, batch_size,
-                                    augment=augment,  # augment images
-                                    hyp=hyp,  # augmentation hyperparameters
-                                    rect=rect,  # rectangular training
-                                    cache_images=cache,
-                                    lazy_caching=lazy_caching,
-                                    cache_efficient_sampling=cache_efficient_sampling,
-                                    rank=rank,
-                                    single_cls=opt.single_cls,
-                                    stride=int(stride),
-                                    pad=pad,
-                                    image_weights=image_weights,
-                                    prefix=prefix)
+    with torch_distributed_zero_first(rank):
+        dataset = LoadImagesAndLabels(path, imgsz, batch_size,
+                                        augment=augment,  # augment images
+                                        hyp=hyp,  # augmentation hyperparameters
+                                        rect=rect,  # rectangular training
+                                        cache_images=cache,
+                                        lazy_caching=lazy_caching,
+                                        cache_efficient_sampling=cache_efficient_sampling,
+                                        rank=rank,
+                                        single_cls=opt.single_cls,
+                                        stride=int(stride),
+                                        pad=pad,
+                                        image_weights=image_weights,
+                                        prefix=prefix)
 
     batch_size = min(batch_size, len(dataset))
     nw = min([os.cpu_count() // world_size, batch_size if batch_size > 1 else 0, workers])  # number of workers
@@ -484,10 +484,10 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
             pbar = enumerate(results)
             pbar = tqdm(pbar, total=n)
             for i, x in pbar:
-                if self.cache_efficient_sampling and rank!=-1:
-                    num_replicas = dist.get_world_size()
-                    if i%num_replicas != rank:
-                        continue
+                # if self.cache_efficient_sampling and rank!=-1:
+                #     num_replicas = dist.get_world_size()
+                #     if i%num_replicas != rank:
+                #         continue
                 self.imgs[i], self.img_hw0[i], self.img_hw[i] = x  # img, hw_original, hw_resized = load_image(self, i)
                 gb += self.imgs[i].nbytes
                 pbar.desc = f'{prefix}Caching images ({gb / 1E9:.1f}GB)'
