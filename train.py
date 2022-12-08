7b84c72b441fbafe01692433e47d35ba63ecf473
davidsvaughn <davidsvaughn@gmail.com>
2022-12-08 16:58:17 -0500

    xxx

--- train.py
+++ train.py
@@ -205,7 +205,7 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
 
     # # Valloader
     val_batch_size = 1
-    # val_batch_size = 4
+    val_batch_size = 4
     val_loader_ddp = create_dataloader(val_path,
                                    imgsz,
                                    val_batch_size, #batch_size // WORLD_SIZE,
